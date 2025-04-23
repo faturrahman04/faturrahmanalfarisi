708cc087b2e01e510b4f9b1a3081d80d5ccb2008
faturrahman04 <faturrahmanalfarisi449@gmail.com>
2025-04-23 19:06:43 +0700

    Menambah komponen baru yaitu CarouselTools dan menambah fitur carousel pada bagian skillsSection.jsx

---
+++ src/components/CarouselTools.jsx
@@ -0,0 +1,12 @@
+const CarouselTools = () => {
+  return (
+    <div className="bg-slate-300 w-fit p-4 rounded-4xl">
+      <div className="bg-white p-6 w-fit rounded-[50%] mb-8">
+        <img className="h-14 w-14" src="./js-logo.png" alt="" />
+      </div>
+      <h2 className="text-center font-medium text-lg font-poppins">JavaScript</h2>
+     </div>
+  )
+}
+
+export default CarouselTools
--- src/section/SkillsSection.jsx
+++ src/section/SkillsSection.jsx
@@ -1,11 +1,26 @@
+import CarouselTools from "../components/CarouselTools";
+
 const SkillsSection = () => {
+
+  const slidesTools = [
+    [
+      <CarouselTools />,
+      <CarouselTools />
+    ], 
+    [
+      <CarouselTools />,
+      <CarouselTools />
+    ]
+  ]
+
+
   return (
-    <section id="skiils" className="bg-gray-300">
+    <section id="skiils" className="bg-gray-200">
       <div className="px-3 lg:px-20 2xl:px-24 pt-6 pb-4">
         <h1>My Skills and Favorite Tools</h1>
 
-        <div>
-
+        <div className="mt-4 px-2 flex">
+          {slidesTools.map((s) => (s))}
         </div>
 
       </div>
