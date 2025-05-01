bd2e375c5d231dd04d085123dc3ca2f67693795c
faturrahman04 <faturrahmanalfarisi449@gmail.com>
2025-05-02 00:48:35 +0700

    fix skills section

--- src/section/SkillsSection.jsx
+++ src/section/SkillsSection.jsx
@@ -4,7 +4,7 @@ const SkillsSection = () => {
 
   
   return (
-    <section id="skiils" className="bg-gray-900">
+    <section id="skills" className="bg-gray-900">
       <div className="px-3 lg:px-20 2xl:px-24 pt-6 pb-12">
         <h1 className="text-xl text-white sm:text-2xl font-poppins font-medium mb-12">My <span className="border-b-2 border-b-orange-400 italic text-orange-400">Skills</span> and Favorite Tools</h1>
 
