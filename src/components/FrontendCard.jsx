83968cb82cd4a42c53ed3e21605512f8dd6cb05f
Faturrahman Alfarisi <157004374+faturrahman04@users.noreply.github.com>
2025-05-13 19:47:12 +0700

    Adjust (#2)
    * add some animation to services and skill card
    
    * Added some meta tag at index.html for SEO optimization

--- src/components/FrontendCard.jsx
+++ src/components/FrontendCard.jsx
@@ -6,8 +6,8 @@ function FrontendCard({
     <img className="rounded object-cover" src={image} alt="" />
 
     <div className="py-4 mt-2 flex flex-wrap gap-4">
-      <a target="_blank" className="border px-4 2xl:px-6 py-2 font-poppins text-sm 2xl:text-xl hover:bg-slate-100 hover:text-black duration-300 rounded-sm" href={linkProject}>View the website</a>
-      <a target="_blank" className="border px-4 py-2 font-poppins text-sm 2xl:text-xl hover:bg-slate-100 hover:text-black duration-300 rounded-sm" href={repository}>GitHub Repository</a>
+      <a target="_blank" className="border px-4 2xl:px-6 py-2 font-winkyRough text-sm 2xl:text-xl hover:bg-slate-100 hover:text-black duration-300 rounded-sm" href={linkProject}>View the website</a>
+      <a target="_blank" className="border px-4 py-2 font-winkyRough text-sm 2xl:text-xl hover:bg-slate-100 hover:text-black duration-300 rounded-sm" href={repository}>GitHub Repository</a>
     </div>
   </div>
 )}
--- src/section/ContactSection.jsx
+++ src/section/ContactSection.jsx
@@ -33,9 +33,9 @@ const ContactSection = () => {
   return (
     <section id="contact" className="bg-gray-900">
       <div className="px-3 lg:px-20 2xl:px-24 py-6">
-        <h1 className="text-white font-medium font-poppins text-xl sm:text-2xl lg:text-5xl">Contact Me</h1>
+        <h1 className="text-white font-medium font-winkyRough text-xl sm:text-2xl lg:text-5xl">Contact Me</h1>
 
-        <h2 className="text-white/75 font-light font-poppins text-sm sm:text-sm lg:text-xl">Thanks for visiting our website, if you want to say something, please input some message into the form below!</h2>
+        <h2 className="text-white/75 font-winkyRough font-light text-sm sm:text-sm lg:text-xl">Thanks for visiting our website, if you want to say something, please input some message into the form below!</h2>
 
         <div>
           <form ref={form} onSubmit={sendEmail}>
@@ -43,14 +43,14 @@ const ContactSection = () => {
             onChange={(e) => setInputName(e.target.value)}
             value={inputName}
             name="name" 
-            className="px-3 py-1.5 w-full rounded-md outline-1 outline-gray-300 text-white placeholder:text-gray-400 focus:outline-2 sm:text-sm/6 focus:outline-orange-500 my-8" 
+            className="px-3 py-1.5 w-full rounded-md outline-1 outline-gray-300 text-white placeholder:text-gray-400 focus:outline-2 sm:text-sm/6 focus:outline-orange-500 my-8 font-winkyRough" 
             type="text" 
             placeholder="Full name" 
             required />
             <p className="absolute text-red-500 -mt-8">{errorMsgName}</p>
             <input name="time" type="datetime-local" className="hidden"/>
-            <textarea name="message" id="message" rows="5" className="block w-full rounded-md bg-transparent px-3 py-1.5 text-base text-white outline-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-orange-500 sm:text-sm/6"></textarea>
-            <button type="submit" className="relative z-10 overflow-hidden bg-transparent w-full rounded-md text-orange-400 py-1.5 font-semibold font-poppins outline-none mt-8 border border-orange-500 hover:text-black after:absolute after:content-[''] after:w-full after:left-0 after:top-0 after:rounded-md after:h-full after:bg-orange-400 after:duration-400 cursor-pointer after:-translate-x-[300%] hover:after:translate-x-0 after:-z-10">Confirm</button>
+            <textarea name="message" id="message" rows="5" className="block w-full rounded-md bg-transparent px-3 py-1.5 text-base text-white outline-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-orange-500 sm:text-sm/6 font-winkyRough"></textarea>
+            <button type="submit" className="relative z-10 overflow-hidden bg-transparent w-full rounded-md text-orange-400 py-1.5 font-semibold font-winkyRough text-lg outline-none mt-8 border border-orange-500 hover:text-black after:absolute after:content-[''] after:w-full after:left-0 after:top-0 after:rounded-md after:h-full after:bg-orange-400 after:duration-400 cursor-pointer after:-translate-x-[300%] hover:after:translate-x-0 after:-z-10">Confirm</button>
           </form>
         </div>
       </div>
