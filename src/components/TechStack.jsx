ecffd138509862e17f6df1be520b9a4d2dc82a5b
faturrahman04 <faturrahmanalfarisi449@gmail.com>
2025-09-17 13:53:15 +0700

    style(project): Menambah projek backend baru 'writhink'

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -10,12 +10,12 @@ export const TechStackMovieApp = () => {
   const imageSize = 'w-10 md:w-12 lg:w-18';
   return (
     <>
-      <img className={`${imageSize}`} src="./react-logo.svg" alt="" />
-      <img className={`${imageSize}`} src="./tailwind-logo.svg" alt="" />
-      <img className={`${imageSize}`} src="./vite.svg" alt="" />
-      <img className={`${imageSize}`} src="./zustand.svg" alt="" />
-      <img className={`${imageSize}`} src="./react-router.svg" alt="" />
-      <img className={`${imageSize}`} src="./axios.svg" alt="" />
+      <img className={`${imageSize}`} src="./react-logo.svg" alt="React" />
+      <img className={`${imageSize}`} src="./tailwind-logo.svg" alt="Talwind" />
+      <img className={`${imageSize}`} src="./vite.svg" alt="Vite" />
+      <img className={`${imageSize}`} src="./zustand.svg" alt="Zustand" />
+      <img className={`${imageSize}`} src="./react-router.svg" alt="React Router" />
+      <img className={`${imageSize}`} src="./axios.svg" alt="Axios" />
     </>
   )
 }
@@ -24,9 +24,9 @@ export const TechStackCoffeShopCaffeIn = () => {
   const imageSize = 'w-10 md:w-12 lg:w-18';
   return (
     <>
-      <img className={`${imageSize}`} src="./html-logo.svg" alt="" />
-      <img className={`${imageSize}`} src="./css-logo.svg" alt="" />
-      <img className={`${imageSize}`} src="./js-logo.png" alt="" />
+      <img className={`${imageSize}`} src="./html-logo.svg" alt="HTML5" />
+      <img className={`${imageSize}`} src="./css-logo.svg" alt="CSS3" />
+      <img className={`${imageSize}`} src="./js-logo.png" alt="JavaScript" />
     </>
   )
 }
@@ -35,9 +35,9 @@ export const TechStackCalculatorApp = () => {
   const imageSize = 'w-10 md:w-12 lg:w-18';
   return (
     <>
-      <img className={`${imageSize}`} src="./html-logo.svg" alt="" />
-      <img className={`${imageSize}`} src="./css-logo.svg" alt="" />
-      <img className={`${imageSize}`} src="./js-logo.png" alt="" />
+      <img className={`${imageSize}`} src="./html-logo.svg" alt="HTML5" />
+      <img className={`${imageSize}`} src="./css-logo.svg" alt="CSS3" />
+      <img className={`${imageSize}`} src="./js-logo.png" alt="Javascript" />
     </>
   )
 }
@@ -46,9 +46,9 @@ export const TechStackToDoListApp = () => {
   const imageSize = 'w-10 md:w-12 lg:w-18';
   return (
     <>
-      <img className={`${imageSize}`} src="./html-logo.svg" alt="" />
-      <img className={`${imageSize}`} src="./css-logo.svg" alt="" />
-      <img className={`${imageSize}`} src="./js-logo.png" alt="" />
+      <img className={`${imageSize}`} src="./html-logo.svg" alt="HTML5" />
+      <img className={`${imageSize}`} src="./css-logo.svg" alt="CSS3" />
+      <img className={`${imageSize}`} src="./js-logo.png" alt="Javascript" />
     </>
   )
 }
@@ -57,10 +57,27 @@ export const TechStackMansapadparLib = () => {
   const imageSize = 'w-10 md:w-12 lg:w-18';
   return (
     <>
-      <img className={`${imageSize}`} src="./php-logo.svg" alt="" />
-      <img className={`${imageSize}`} src="./css-logo.svg" alt="" />
-      <img className={`${imageSize}`} src="./js-logo.png" alt="" />
-      <img className={`${imageSize}`} src="./codeigniter-logo.svg" alt="" />
+      <img className={`${imageSize}`} src="./php-logo.svg" alt="PHP" />
+      <img className={`${imageSize}`} src="./css-logo.svg" alt="CSS3" />
+      <img className={`${imageSize}`} src="./js-logo.png" alt="Javascript" />
+      <img className={`${imageSize}`} src="./codeigniter-logo.svg" alt="Codeigniter" />
+      <img className={`${imageSize}`} src="./mysql-logo.svg" alt="MySQL" />
+    </>
+  )
+}
+
+export const TechStackWrithink = () => {
+  const imageSize = 'w-10 md:w-12 lg:w-18';
+  return (
+    <>
+      <img className={`${imageSize}`} src="./react-logo.svg" alt="React" />
+      <img className={`${imageSize}`} src="./tailwind-logo.svg" alt="Tailwind" />
+      <img className={`${imageSize}`} src="./nodejs-logo.svg" alt="NodeJS" />
+      <img className={`${imageSize}`} src="./express-logo.svg" alt="ExpressJS" />
+      <img className={`${imageSize}`} src="./mysql-logo.svg" alt="MySQL" />
+      <img className={`${imageSize}`} src="./vite.svg" alt="Vite" />
+      <img className={`${imageSize}`} src="./react-router.svg" alt="React router" />
+      <img className={`${imageSize}`} src="./zustand.svg" alt="Zustand" />
     </>
   )
 }
